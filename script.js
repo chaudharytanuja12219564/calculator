9919100246fc80feeb8c6a3f6c0b8305fb29ab1c
Tanuja Chaudhary 12219564 <119854154+chaudharytanuja12219564@users.noreply.github.com>
2024-07-07 14:58:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let string="";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+
+        if(e.target.innerHTML=='='){
+            string = eval(string);
+            document.querySelector('input').value= string;
+        }
+
+        else if(e.target.innerHTML=='AC'){
+            string = "";
+            document.querySelector('input').value= string;
+        }
+
+        else if(e.target.innerHTML=='√'){
+            try {
+                string = Math.sqrt(parseFloat(string)).toString();
+                document.querySelector('input').value = string;
+            } catch (error) {
+                document.querySelector('input').value = "Error";
+                string = "";
+            }
+        }
+        
+        else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value= string;
+        }
+        
+    })
+})
